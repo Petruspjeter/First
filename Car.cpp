7f18efb4bf95099af5222943c2321a439c6c09be
Petruspjeter <piotr.blazdziewicz.stud@pw.edu.pl>
2020-04-02 01:07:15 +0200

    Pierwszy commit przez CLiona

---
+++ Car.cpp
@@ -0,0 +1,14 @@
+//
+// Created by pblaz on 01.04.2020.
+//
+#include <iostream>
+#include "Car.h"
+
+using namespace std;
+
+void Car::jazda(int r, int k)
+    {
+        cout<<"Jedziemy!"<<endl;
+        cout<<"Kola: "<<k<<endl;
+        cout<<"Rocznik: "<<r<<endl;
+    }
---
+++ Car.h
@@ -0,0 +1,19 @@
+//
+// Created by pblaz on 01.04.2020.
+//
+
+#ifndef FIRST_CAR_H
+#define FIRST_CAR_H
+
+
+class Car
+{
+public:
+    int rocznik;
+    int rozmiarKol;
+
+    void jazda(int, int);
+};
+
+
+#endif //FIRST_CAR_H
